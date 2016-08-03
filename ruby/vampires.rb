0327d258abaa22c17e8a8518c6de04f666cb396b
Aidan Legaspi <aidanlegaspi1@gmail.com>
2016-08-03 13:38:11 -0700

    wrote program that asks questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+user_name = gets.chomp
+puts "How old are you?"
+user_age = gets.chomp
+puts "What year were you born?"
+year_of_birth = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company’s health insurance? y/n"
+health_insurance = gets.chomp
