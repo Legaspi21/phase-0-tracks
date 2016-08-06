e06543807dc456a245d235d625f7329c7219a40c
Aidan Legaspi <aidanlegaspi1@gmail.com>
2016-08-06 10:31:56 -0700

    pasted in the working decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -18,14 +18,19 @@
 #puts string
 
 def encrypt (string)
-	i = 0
-	while i < string.length
-	string[i] = string[i].next
-	i += 1 
-end
+        i = 0
+        while i < string.length
+          if string[i] == "z"                 #added edge case conditional
+            string[i] = "a"
+          else
+    string[i] = string[i].next
+    end
+    i += 1
+  end
 p string
 end
 encrypt("abc")
+encrypt("zed")
 
 #A decrypt method that reverses the process above. Note that Ruby doesn't
 #have a built-in method for going backward one letter. How can you find 
@@ -33,17 +38,17 @@ encrypt("abc")
 #Hint: In the IRB session above, you learned how to ask a string for the index of a letter.
 #"abcdefghijklmnopqrstuvwxyz" counts as a string.
 
-#Establish a string variable (alpha) with the value of "abcdefghijklmnopqrstuvwxyz"
-def decrypt (encrypted_string)
-	i = 0
-	while i < encrypted_string.length
-	encrypted_string[i]
-	num = "abcdefghijklmnopqrstuvwxyz".index encrypted_string[i]
-	num2 = num -1
-	num3 = "abcdefghijklmnopqrstuvwxyz"[num2]
-	puts num3
-	i += 1
-	end
+def decrypt(encrypted_string)
+  i = 0
+  num = "abcdefghijklmnopqrstuvwxyz"
+  while i < encrypted_string.length
+    place = encrypted_string[i]         #place is letter in encrypted_string
+    new_num = num.index(place)          #new_num is the index in num of letter
+    encrypted_string[i] = num[new_num - 1]
+    #p encrypted_string                 #print each new string for test
+    i += 1
+  end
+p encrypted_string
 end
-
-decrypt("qbttxpse")
+decrypt("bcd")
+decrypt("afe")
