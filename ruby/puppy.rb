098ced1f863db314ceac4c7441e7a966719ccced
Aidan Legaspi <aidanlegaspi1@gmail.com>
2016-08-15 16:10:08 -0700

    designed a class for puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Puppus doggus minimus
+
+# CHARACTERISTICS ----------------
+# Size: varies
+# Sex: varieS
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Fetch
+# Rollover
+# Chase Tail
