d2ee3de4ff15888b4e2f630aab501b369accb5ce
Aidan Legaspi <aidanlegaspi1@gmail.com>
2016-08-20 11:47:01 -0700

    Added yelling happily method and test calls

---
+++ ruby/shout.rb
@@ -0,0 +1,14 @@
+module Shout
+
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "!!!" + " :)"
+  end
+
+end
+
+Shout.yell_angrily("Quiet")
+Shout.yelling_happily("Hurray")
