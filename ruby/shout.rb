85b887f67ff3d4e3887c4ca7750f0331ae216c6d
Aidan Legaspi <aidanlegaspi1@gmail.com>
2016-08-20 12:14:21 -0700

    Created mixin version of Shout and added classes and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,42 @@
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + "!!!" + " :)"
+#   end
+
+# end
+
+# Shout.yell_angrily("Quiet")
+# Shout.yelling_happily("Hurray")
+
 module Shout
 
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words + "!!!" + " :)"
   end
 
 end
 
-Shout.yell_angrily("Quiet")
-Shout.yelling_happily("Hurray")
+class Human
+  include Shout
+end
+
+class Parrot
+  include Shout
+end
+
+human = Human.new
+p human.yell_angrily("I am so upset")
+p human.yelling_happily("Oh happy day")
+
+parrot = Parrot.new
+p parrot.yell_angrily("Wheres my cracker?")
+p parrot.yelling_happily("Time to eat")
