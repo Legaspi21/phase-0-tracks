789c936f8a1b0226a5e59c7a8a2d2624911549a9
Aidan Legaspi <aidanlegaspi1@gmail.com>
2016-08-26 21:10:00 -0700

    Created function that returns longest word or phrase in an array. Added test driver code.

--- js/algos.js
+++ js/algos.js
@@ -10,7 +10,21 @@ Also add the item at the current index to the item variable
 Return the item with the greatest length at the end of function
 */
 
+function longestItem(array){
+	var length = 0;
+	var item = '';
+	for (i = array.length-1; i >= 0; i--){
+		if (array[i].length >= length){
+			length = array[i].length;
+			item = array[i];
+		}
+	}
+	return item;
+}
 
+console.log(longestItem(["Britney Spears", "N'Sync", "Backstreet Boys",]));
+console.log(longestItem(["Hello","Good Bye Larry","Umm what is this?","Geez Louis", "Halleluja Howdy Do Ya", "By Golly Oh My", "Oh My Word How?", "And Thats It", "Thats All", "Folks"]));
+console.log(longestItem(["Barking Up The Wrong Tree","Knuckle Down","Goody Two-Shoes","Wake Up Call"]))
 
 
 
