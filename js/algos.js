e248b0ba05384b73ab0b51070750051eae0179f6
Aidan Legaspi <aidanlegaspi1@gmail.com>
2016-08-26 17:35:56 -0700

    Pseudocode for a function that returns the longest string in an array

---
+++ js/algos.js
@@ -0,0 +1,16 @@
+/* Pseudocode 
+Create function that takes an array as a parameter
+Create a variable for length and set it to 0
+Create a variable for item to store the longest item
+Loop through array
+For each item in the array find the length of that item
+For each item evaluate if it is longer than the value of length
+If it is longer add the length to length variable
+Also add the item at the current index to the item variable
+Return the item with the greatest length at the end of function
+*/
+
+
+
+
+
